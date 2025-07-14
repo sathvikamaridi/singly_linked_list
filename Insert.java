53d1ea2ce323ac033b9e68f7511472e6c6d827c6
sathvikamaridi <120960220+sathvikamaridi@users.noreply.github.com>
2025-07-14 09:14:28 +0530

    Add basic singly linked list insertion and display implementation

---
+++ Insert.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Insert
+{
+    static class SLLNode 
+    {
+        int data;
+        SLLNode next;
+        public SLLNode()
+        {
+          this.data=0;
+          this.next=null;
+        }
+        public SLLNode(int data, SLLNode next)
+        {
+          this.data=data;
+          this.next=next;
+        }
+    }
+        static SLLNode insert_at_end(SLLNode head, int input) 
+        {
+            SLLNode newNode = new SLLNode(input,null);
+            if (head == null) 
+            {
+                head = newNode;
+            } 
+            else 
+            {
+                SLLNode temp = head;
+                while (temp.next != null) 
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            return head;
+        }
+        static void sll_display(SLLNode head)
+      {
+         SLLNode tptr;
+         for(tptr=head;tptr!=null;tptr=tptr.next)
+        {
+            System.out.print(tptr.data+" ");
+        }
+      }
+    public static void main(String[] args) 
+    {
+        SLLNode head=null;
+        Scanner scan = new Scanner(System.in);
+        while(true)
+        {
+            int num=scan.nextInt();
+            if(num==-1)
+                break;
+            head=insert_at_end(head,num);
+        }
+        sll_display(head);
+        scan.close();
+    }
+}
